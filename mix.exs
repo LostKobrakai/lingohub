f18af24f559f955b4556bb2396cfb622b6b0ce98
Benjamin Milde <benni@kobrakai.de>
2019-01-22 14:05:02 +0100

    Add details for hex publishing

--- mix.exs
+++ mix.exs
@@ -8,7 +8,11 @@ defmodule LingoHub.MixProject do
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      lingo_hub: lingo_hub()
+      description: description(),
+      package: package(),
+      lingo_hub: lingo_hub(),
+      name: "LingoHub",
+      source_url: "https://github.com/LostKobrakai/lingohub"
     ]
   end
 
@@ -25,6 +29,20 @@ defmodule LingoHub.MixProject do
       {:httpoison, "~> 1.4"},
       {:gettext, "~> 0.16.0", optional: true},
       {:jason, "~> 1.1", optional: true}
+    ]
+  end
+
+  defp description do
+    "Integration with the LingoHub API and some nice mix tasks to integrate with gettext."
+  end
+
+  defp package do
+    [
+      # These are the default files included in the package
+      files: ~w(lib .formatter.exs mix.exs README* LICENSE*
+                CHANGELOG*),
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/LostKobrakai/lingohub"}
     ]
   end
 
